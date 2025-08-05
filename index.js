d3c3f5bd798d12936811688ab4a5ab86973fde82
Miguel Colque <micky.cm@outlook.es>
2025-08-04 21:13:29 -0400

    Ejercicio 3.7 - Parte 3: Agregar middleware morgan con la configuración tiny para mostrar mensajes en la consola

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -92,3 +96,9 @@ app.post('/api/persons', (request, response) => {
 
   response.status(201).json(newPerson)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
