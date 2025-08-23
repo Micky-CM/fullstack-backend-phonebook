bdf26420a60bdd4b845c589843d9fe4769673c4f
Miguel Colque <micky.cm@outlook.es>
2025-08-23 19:38:00 -0400

    Ejercicios 3.15 y 3.16 - Parte 3: Cambio del backend para que eliminar entradas y manejar errores en un nuevo middleware

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -82,52 +82,62 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).end()
-    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-  .then(response => {
+  .then(result => {
     response.status(204).end()
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).end()
-  })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  /* const nameExists = Person.findOne(body.name)
 
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing'
     })
   }
-  if (nameExists) {
-    return response.status(400).json({
-      error: 'name must be unique'
+
+  Person.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({
+          error: 'name must be unique'
+        })
+      }
+
+      const newPerson = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      return newPerson.save()
     })
-  } */
-
-  const newPerson = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  newPerson.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-
-  response.status(201).json(newPerson)
+    .then(savedPerson => {
+      if (savedPerson) {
+        response.status(201).json(savedPerson)
+      }
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint'})
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'DocumentNotFoundError') {
+    return response.status(404).send({ error: 'person not found'})
+  }
+  next(error)
+}
+app.use(errorHandler)
