62818f7166397d341b74bae15f5b504d5b69588a
Miguel Colque <micky.cm@outlook.es>
2025-09-13 15:23:52 -0400

    Ejercicio 3.20 - Parte 3: Validación ampliada para asegurar que los números de teléfono tengan el formato correcto

--- models/Person.js
+++ models/Person.js
@@ -6,7 +6,17 @@ const personSchema = new mongoose.Schema({
     minlength: [3, 'Name must be at least 3 characters long'],
     required: [true, 'Name is required']
   },
-  number: String
+  number: {
+    type: String,
+    minlength: [8, 'Number must be at least 8 characters long'],
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{3,8}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! It should be in the format XX-XXXXXXX or XXX-XXXXXXX`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
